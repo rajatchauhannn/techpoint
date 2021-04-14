580d10810f721d3bc9f97c5e45105aff68a763ee
Rajat Chauhan <rajat1234chauhan@gmail.com>
2021-04-14 20:40:04 +0530

    google map added but still needs to be fuxed

--- src/Contact.js
+++ src/Contact.js
@@ -1,51 +1,62 @@
 import React from 'react';
 import styled from 'styled-components';
+import MapContainer from './components/GoogleMap';
 
+const Map = styled.div`
+
+`;
+
+const Row = styled.div`
+    diplay:flex;
+    color: white;
+    justify-content: center;
+`;
+
+const Column = styled.div`
+    flex: 50%;
+    width: 50%;
+    padding: 100px;
+`;
 
 const Heading = styled.div`
 
     font-size: 30px;
-`;
-
-const GridWrapper = styled.div`
-    width: 100px;
-    margin: 0 20%;
-    
-    display: grid;
     color: white;
-    justify-content: right;
-    grid-gap: 50px 50px;
-  
 `;
-
 
 export const Contact = (props) => (
     <>
-    <GridWrapper>
+        <Heading>Contact Us</Heading>
+        <Row>
+            <Column>
+                <p>Corporate office:
+                    E-6, 2nd Floor, Block-E,
+                    Main Road, Kalkaji, 
+                    New Delhi-110019
+                    <hr></hr>
+                    Mb:+91 965010 1559 
+                        +91 997171 2955 
+                    Ph: +91 11-47025933
+                    Email: kuldeep@techpointitsolutions.in
+                    ujjwal@techpointitsolutions.in</p>
+            <Column>
+            <Map>
+                <MapContainer />
+            </Map>
+            </Column>
+                <p>Registered office:
+                    171, 2nd Floor, Sector-5,
+                    Vasundhara, Ghaziabad,
+                    Uttar Pradesh-201012.
+                    <hr></hr>
+                    Mb:+91 965010 1559 
+                        +91 997171 2955 
+                    Ph: +91 120-4575953
+                    Email: kuldeep@techpointitsolutions.in
+                    ujjwal@techpointitsolutions.in</p>              
+            </Column>
+        </Row>
 
-      <Heading>Contact Us</Heading>
-      <p>Corporate office:
-            E-6, 2nd Floor, Block-E,
-            Main Road, Kalkaji, 
-            New Delhi-110019
-            <hr></hr>
-            Mb:+91 965010 1559 
-                +91 997171 2955 
-            Ph: +91 11-47025933
-            Email: kuldeep@techpointitsolutions.in
-            ujjwal@techpointitsolutions.in</p>
-      <p>Registered office:
-            171, 2nd Floor, Sector-5,
-            Vasundhara, Ghaziabad,
-            Uttar Pradesh-201012.
-            <hr></hr>
-            Mb:+91 965010 1559 
-                +91 997171 2955 
-            Ph: +91 120-4575953
-            Email: kuldeep@techpointitsolutions.in
-            ujjwal@techpointitsolutions.in</p>
-      
-    </GridWrapper>
     </>
 
 )
