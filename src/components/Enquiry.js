e75190ce201f1bd84d068d7da5dd50079c9274b8
Rajat Chauhan <rajat1234chauhan@gmail.com>
2021-04-28 05:56:34 +0530

    Emailing functionality aded to the query Section

--- src/components/Enquiry.js
+++ src/components/Enquiry.js
@@ -1,29 +1,93 @@
 import React from 'react';
 import { Form, Col, Button } from 'react-bootstrap';
+import { useForm } from "react-hook-form";
 
-export const Enquiry = (props) => (
-    <Form>
-        <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Name*</Form.Label>
-            <Form.Control type="name" placeholder="Name" />
-        </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Label>Email*</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-        <Form.Group as={Col} controlId="formGridPhone">
-            <Form.Label>Mobile no.*</Form.Label>
-            <Form.Control placeholder="" />
-        </Form.Group>
+const initialFormData = Object.freeze({
+    username: "",
+    email: "",
+    mobile: "",
+    query: ""
+  });
+  
 
-        <Form.Group as={Col} id="formGridFeedback">
-            <Form.Label>Query*</Form.Label>
-            <Form.Control as="textarea" rows={3} />
-        </Form.Group>
+export const Enquiry = (props) => {
+    const { register } = useForm();
+    const [formData, updateFormData] = React.useState(initialFormData);
 
-        <Button variant="primary" type="submit">
-            Submit
-        </Button>
-    </Form>
-)
+    const sendFeedback = (serviceID, templateId, variables) => {
+        window.emailjs.send(
+            serviceID, templateId,
+            variables
+        ).then(res => {
+            console.log('Email successfully sent!')
+        })
+            .catch(err => console.error('There has been an error.  Here some thoughts on the error that occured:', err))
+    }
+    
+
+    const handleChange = (e) => {
+        updateFormData({
+          ...formData,
+
+          [e.target.name]: e.target.value.trim()
+        });
+      };
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        alert(`Thank you for your message. Your query has been forwarded.`);
+        const templateId = 'template_4rd43a2';
+        const serviceID = "service_agey6km";
+        sendFeedback(serviceID, templateId, { from_name: formData.name, message_html: formData.query, reply_to: formData.email })
+
+        console.log(formData);
+      };
+
+    return (
+
+        <Form id="enquiry">
+            <Form.Group as={Col} controlId="formGridName">
+                <Form.Label>Name*</Form.Label>
+                <Form.Control onChange={handleChange} name="name" type="name" placeholder="Name" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Email*</Form.Label>
+                <Form.Control onChange={handleChange} name="email" type="email" placeholder="Enter email"
+                    ref={
+                        register(
+                            "email", {
+                            required: "Please enter an email",
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: "invalid email address"
+                            }
+                        })
+                    }
+                />
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridMobile">
+                <Form.Label>Mobile no.*</Form.Label>
+                <Form.Control onChange={handleChange} name="mobile" placeholder="" />
+            </Form.Group>
+            <Form.Group as={Col} id="formGridQuery">
+                <Form.Label>Query*</Form.Label>
+                <Form.Control onChange={handleChange} name="query" as="textarea" rows={3}
+                    ref={
+                        register(
+                            "query", {
+                            required: true
+                        }
+                        )
+                    }
+                />
+            </Form.Group>
+
+            <Button onClick={handleSubmit} variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form >
+
+    )
+}
