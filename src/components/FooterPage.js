6b466f19e522059c37483d4d4e14700711df1e10
Rajat Chauhan <rajat1234chauhan@gmail.com>
2021-04-30 08:14:55 +0530

    fixed

--- src/components/FooterPage.js
+++ src/components/FooterPage.js
@@ -13,7 +13,7 @@ export const FooterPage = () => {
           Techpoint IT Solutions: Deal in Desktops, Laptops, All in One PCs, Servers, Workstations, Printers, UPS and Software’s. Cloud Co-Location and Security Services, etc.
       </FooterHeading>
       </Box>
-      <FooterText>   © 2019 - {new Date().getFullYear()} Techpoint IT Solutions. All Rights Reserved. | <a href="">Privacy Policy</a> | <a href=""> Terms of Use</a>
+      <FooterText>   © 2019 - {new Date().getFullYear()} Techpoint IT Solutions. All Rights Reserved. | <a href="/">Privacy Policy</a> | <a href="/"> Terms of Use</a>
       </FooterText>
      
     </>
