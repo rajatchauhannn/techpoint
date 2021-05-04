3fa4434b986c4fbdc07d998fba4fd6de2abce9a6
Rajat Chauhan <rajat1234chauhan@gmail.com>
2021-05-04 21:33:53 +0530

    Created new pages for solutions, refined dropdown menu, routed them all, logo is now working on all pages

---
+++ src/solutions/Storage.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+`;
+
+export default class Storage extends Component {
+    render() {
+        return (
+            <Wrapper>
+                <p> Data, Data Everywhere
+                Today’s organizations confront an explosion of data captured and
+                collected from a multitude of sources across and beyond the enterprise.
+                Managing and controlling this massive amount of information presents a
+                significant challenge, especially for businesses whose data growth far
+                exceeds internal resources
+                Culling from decades of practical experience implementing information
+                technology and systems, Techpoint IT Solutions. will help you manage and
+                mine your data to make faster, better business decisions while reducing
+                costs. Beyond delivering storage solutions, we deliver the expertise to:
+                • Maximize your current storage investments and minimize storage costs
+                • Classify and assign data to appropriate storage solutions based on the
+                level of sensitivity and accessibility requirements
+                • Maximize the cost- and space-savings of data de-duplication
+                • Manage your digital assets, determining what to preserve and what to delete
+                • Provision who has access to what data, under what conditions
+                • Back up your data and secure it from unauthorized access and malicious use
+                • Define and implement processes to control data growth
+                • Access and leverage information to improve efficiencies and operations
+                </p>
+            </Wrapper>
+        )
+    }
+}
+
+
+
---
+++ src/solutions/Virtualization.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+`;
+
+export default class Virtualization extends Component {
+    render() {
+        return (
+            <Wrapper>
+                <p> Virtualization Realized
+                Many organizations have embraced the concept
+                of virtualization, only to encounter the myriad
+                of complexities inherent in managing a virtual environment.
+                At Techpoint IT Solutions., we not only ease the transition
+                to virtualization, but our expertise extends well beyond
+                the application layer, enabling us to optimize the way you
+                manage — and maximize — your entire environment.
+
+                Through careful assessment and evaluation, we make sure we
+                understand how your current virtualization investments
+                impact your people, processes and systems. Leveraging
+                this insight, we’ll help you make the right decisions
+                about where and how to virtualize so that you see the
+                greatest return from your investment, across the enterprise
+                — from the desktop to the network. We’ll build the proper
+                framework to support your virtualization and consolidation
+                initiatives. What’s more, we’ll help you develop and enact
+                the processes, tools and procedures that promote the long-term
+                success of your virtualization strategy. In short, we make the
+                promise of virtualization a business reality.
+                </p>
+            </Wrapper>
+        )
+    }
+}
+
+
+
