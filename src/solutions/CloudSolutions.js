897846d9747754b1dcfb3f5d8a3a9a9f03f5a2b0
Rajat Chauhan <rajat1234chauhan@gmail.com>
2021-05-08 11:21:51 +0530

    Added a sidebar to solutions page

--- src/solutions/CloudSolutions.js
+++ src/solutions/CloudSolutions.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import ProgressiveImage from '../components/ProgressiveImage';
 import { Wrapper } from './Styles';
-
+import { Col, Row } from 'react-bootstrap';
+import SideBar from '../components/SideBar';
 
 export default class CloudSolutions extends Component {
     render() {
@@ -16,20 +17,27 @@ export default class CloudSolutions extends Component {
                         src="/img/solutions/Cloud Solutions.jpg"
                     />
                 </div>
-                <p> Leverage the Hybrid Cloud
-                Empower your business with Cloud Solutions from Techpoint IT Solutions..
-                Designed to enable your organization with more agile, cost effective
-                technologies, our Cloud services and solutions span many different
-                topologies and varying levels of capability to address your
-                organization's present-day need.
-                • Higher utilization of resources
-                • Significantly decrease time to complete service requests
-                • Financially and technically better solutions for risk mitigation and
-                increased business agility
-                • Easily migrate to your hybrid cloud solution based on our proven assessment process
-                TAKE OUR ONE CUBE ASSESSMENT HERE or email info@techpointitsolutions.com
-                to know more about our cloud solutions.
-                </p>
+                <Row>
+                    <Col xs={9}>
+                        <p> Leverage the Hybrid Cloud
+                        Empower your business with Cloud Solutions from Techpoint IT Solutions..
+                        Designed to enable your organization with more agile, cost effective
+                        technologies, our Cloud services and solutions span many different
+                        topologies and varying levels of capability to address your
+                            organization's present-day need.<br /><br />
+                            • Higher utilization of resources<br />
+                            • Significantly decrease time to complete service requests<br />
+                            • Financially and technically better solutions for risk mitigation and
+                            increased business agility<br />
+                            • Easily migrate to your hybrid cloud solution based on our proven assessment process
+                            Email info@techpointitsolutions.com
+                            to know more about our cloud solutions.<br />
+                        </p>
+                    </Col>
+                    <Col>
+                    <SideBar />
+                    </Col>
+                </Row>
             </Wrapper>
         )
     }
--- src/solutions/Storage.js
+++ src/solutions/Storage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ProgressiveImage from '../components/ProgressiveImage';
 import { Wrapper } from './Styles';
+import SideBar from '../components/SideBar';
+import { Col, Row } from 'react-bootstrap';
 
 export default class Storage extends Component {
     render() {
@@ -15,26 +17,33 @@ export default class Storage extends Component {
                         src="/img/solutions/Storage.jpg"
                     />
                 </div>
-                <p> Data, Data Everywhere
-                Today’s organizations confront an explosion of data captured and
-                collected from a multitude of sources across and beyond the enterprise.
-                Managing and controlling this massive amount of information presents a
-                significant challenge, especially for businesses whose data growth far
-                exceeds internal resources
-                Culling from decades of practical experience implementing information
-                technology and systems, Techpoint IT Solutions. will help you manage and
-                mine your data to make faster, better business decisions while reducing
-                costs. Beyond delivering storage solutions, we deliver the expertise to:
-                • Maximize your current storage investments and minimize storage costs
-                • Classify and assign data to appropriate storage solutions based on the
-                level of sensitivity and accessibility requirements
-                • Maximize the cost- and space-savings of data de-duplication
-                • Manage your digital assets, determining what to preserve and what to delete
-                • Provision who has access to what data, under what conditions
-                • Back up your data and secure it from unauthorized access and malicious use
-                • Define and implement processes to control data growth
-                • Access and leverage information to improve efficiencies and operations
+                <Row>
+                    <Col xs={9}>
+                        <p> Data, Data Everywhere
+                        Today’s organizations confront an explosion of data captured and
+                        collected from a multitude of sources across and beyond the enterprise.
+                        Managing and controlling this massive amount of information presents a
+                        significant challenge, especially for businesses whose data growth far
+                        exceeds internal resources
+                        Culling from decades of practical experience implementing information
+                        technology and systems, Techpoint IT Solutions. will help you manage and
+                        mine your data to make faster, better business decisions while reducing
+                        costs. Beyond delivering storage solutions, we deliver the expertise to: <br /><br />
+                        • Maximize your current storage investments and minimize storage costs<br />
+                        • Classify and assign data to appropriate storage solutions based on the
+                        level of sensitivity and accessibility requirements<br />
+                        • Maximize the cost- and space-savings of data de-duplication<br />
+                        • Manage your digital assets, determining what to preserve and what to delete<br />
+                        • Provision who has access to what data, under what conditions<br />
+                        • Back up your data and secure it from unauthorized access and malicious use<br />
+                        • Define and implement processes to control data growth<br />
+                        • Access and leverage information to improve efficiencies and operations<br />
                 </p>
+                    </Col>
+                    <Col>
+                        <SideBar />
+                    </Col>
+                </Row>
             </Wrapper>
         )
     }
